e9f2bab329e12e76ecac52e222fb7af0bd44633f
rodionovd <i.am.rodionovd@gmail.com>
2015-01-18 00:51:52 +0600

    Turn the app into Dockless mode

--- Ristretto/AppDelegate.m
+++ Ristretto/AppDelegate.m
@@ -9,10 +9,6 @@
 #import "AppDelegate.h"
 #import <AVFoundation/AVFoundation.h>
 
-@interface AppDelegate ()
-
-@end
-
 @implementation AppDelegate
 {
     AVAudioPlayer *audioPlayer;
@@ -22,15 +18,13 @@
     // Insert code here to initialize your application
     _statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
 
-    NSImage *menuIcon       = [NSImage imageNamed:@"Status"];
-    NSImage *highlightIcon  = [NSImage imageNamed:@"StatusHighlighted"];
+    NSImage *menuIcon = [NSImage imageNamed:@"Status"];
+    [menuIcon setTemplate:YES];
 
     [[self statusItem] setImage:menuIcon];
-    [[self statusItem] setAlternateImage:highlightIcon];
     [[self statusItem] setHighlightMode:YES];
-    [[self statusItem] setTarget:self];
-    [[self statusItem] setAction:@selector(coffeeshopChillTime:)];
-
+    [[self statusItem] setMenu: [self statusItemMenu]];
+    
     NSString* path = [[NSBundle mainBundle] pathForResource:@"murmur" ofType:@"mp3"];
     NSURL* file = [NSURL fileURLWithPath:path];
     audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:file error:nil];
@@ -38,19 +32,58 @@
     audioPlayer.numberOfLoops = -1; //Infinite chilloop
 }
 
+- (NSMenu *)statusItemMenu {
+    NSMenuItem *toggleItem = ({
+        NSMenuItem *item = [NSMenuItem new];
+        item.title = NSLocalizedString(@"Pour the chill", @"Status bar menu item");
+        item.onStateImage = [NSImage imageNamed: @"Pause"];
+        item.onStateImage.template = YES;
+        item.offStateImage = [NSImage imageNamed: @"Play"];
+        item.offStateImage.template = YES;
+        
+        item.action = @selector(coffeeshopChillTime:);
+        item.state = NSOffState;
+        item;
+    });
+    
+    NSMenuItem *quitItem = ({
+        NSMenuItem *item = [NSMenuItem new];
+        item.title = NSLocalizedString(@"Quit", @"Status bar menu item");
+        item.action = @selector(terminate:);
+        item.keyEquivalent = @"q";
+        item;
+    });
+
+    NSMenu *menu = [NSMenu new];
+    [menu addItem: toggleItem];
+    [menu addItem: [NSMenuItem separatorItem]];
+    [menu addItem: quitItem];
+    
+    return menu;
+}
+
 - (void)coffeeshopChillTime:(id)sender {
+
+    NSMenuItem *item = (NSMenuItem *)sender;
+    
     if ([audioPlayer isPlaying]) {
         [audioPlayer pause];
         NSLog(@"Pause the chill!");
+        
+        item.title = NSLocalizedString(@"Pour the chill", @"Status bar menu item");
+        item.state = NSOffState;
     } else {
         [audioPlayer play];
         NSLog(@"Pour the chill!");
+        
+        item.title = NSLocalizedString(@"Pause the chill", @"Status bar menu item");
+        item.state = NSOnState;
     }
 }
 
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+    [[NSStatusBar systemStatusBar] removeStatusItem: self.statusItem];
 }
 
 @end
